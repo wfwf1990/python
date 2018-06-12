2a4e3a2a60a7c4ed46f966c42afa9fc67b40c7c8
wfwf1990 <576589099@qq.com>
2018-06-12 22:37:04 +0800

    day4

--- Day4/dict(字典).py
+++ Day4/dict(字典).py
@@ -16,4 +16,42 @@ dict1 = {"tom":60,"jack":70}
 dict2 = {"tom":60,"jack":70}
 print(dict2["tom"])
 
-print(dict2.get("tom"))
+print(dict2.get("tom1")) #key不存在,返回none
+
+ret = dict2.get("tom1")
+if ret == None:
+    print("没有!")
+
+#（3）添加
+#一个key对应一个value,所以多次对一个key的value赋值,其实就是修改值
+dict3 = {"tom":60,"jack":70}
+dict3["wf"] = 80
+print(dict3)
+
+#（4）删除
+#根据key删除
+dict4 = {"tom":60,"jack":70}
+dict4.pop("tom")
+print(dict4)
+
+#（5）字典遍历
+#1）方式一：
+dict5 = {"tom":60,"jack":70}
+for i in dict5:
+    print(i,dict5[i])
+
+#2）遍历value
+dict6 = {"tom":60,"jack":70}
+print(dict6.values())   #[60, 70]
+for i in dict6.values():
+    print(i)
+#3）
+dict7 = {"tom":60,"jack":70}
+print(dict7.items())        #[('jack', 70), ('tom', 60)]
+for k,v in dict7.items():
+    print(k,v)
+
+#4）
+dict8= {"tom":60,"jack":70}
+for k,v in enumerate(dict8):
+    print(k,v)
