c6b29303b454705a3d9069b4e308111cc18d9d2b
admin <576589099@qq.com>
2018-06-19 17:45:45 +0800

    模块

---
+++ Day6/11-模块/8丶安装第三方模块.py
@@ -0,0 +1,16 @@
+
+
+
+
+#操作系统需要提前安装好pip软件，路径添加到系统的path环境变量中
+#windows环境变量马上生效：set Path=     查看echo %Path%
+#pip -V   查看pip版本
+
+#要安装三方模块,需要知道模块的名字
+#pillow 非常强大的处理图像的工具库
+
+#安装： pip install pillow
+
+#引入了三方库
+from PIL import Image           #处理图片
+
