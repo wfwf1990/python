6fb506044ee9ce0b5b61bc7a1e32a6111822ecd4
admin <576589099@qq.com>
2018-06-21 19:04:06 +0800

    类

--- Day6/12-类/9丶继承/单继承的实现/单继承的实现.py
+++ Day6/12-类/9丶继承/单继承的实现/单继承的实现.py
@@ -3,7 +3,7 @@ from worker import Worker
 stu = Student("tom",18,1000,19)
 print(stu.name,stu.age,stu.stuID)
 stu.run()
-print(stu.getMoney())   #通过方法获取父类的私有属性
+print(stu.getMoney())   #通过继承过来的公有方法访问私有属性
 
 wor = Worker("jack",19,1000)
 print(wor.name,wor.age)
